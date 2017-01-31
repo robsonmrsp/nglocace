0d2131a3389a3e80fee6a67bad59ef733ad3fdb7
Robson <robsonmrsp@gmail.com>
2017-01-31 16:51:23 -0200

    Criada a infra de servico para genero com o componente utilizando o servico e devolvendo dados.

--- src/app/genero/genero.component.ts
+++ src/app/genero/genero.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GeneroService } from './genero.service';
 @Component( {
     selector: 'app-genero',
     templateUrl: './genero.component.html',
-    styleUrls: ['./genero.component.css']
+    styleUrls: ['./genero.component.css'],
+    providers: [GeneroService]
 })
 export class GeneroComponent implements OnInit {
 
-    constructor() { }
+    generos: any[] = [];
 
-    generos: any[] = [{ nome: 'robson', descricao: 'teste' }, { nome: 'robson2', descricao: 'teste2' }];
+    constructor( private generoService: GeneroService ) { }
+
+    getGeneros() {
+        this.generoService.getAll().then(( generos: any[] ) => this.generos = generos );
+    }
 
     ngOnInit() {
-
+        this.getGeneros();
     }
 
 }
---
+++ src/app/genero/genero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GeneroService {
+
+    constructor() { }
+
+
+
+
+    getAll(): Promise<any> {
+        return Promise.resolve( [{ nome: 'robson', descricao: 'teste' }, { nome: 'robson2', descricao: 'teste2' }] );
+    }
+}
