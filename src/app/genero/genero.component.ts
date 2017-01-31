c9129d19e5a43b37d70c0ed7be5c410e3d7ae4ea
Robson <robsonmrsp@gmail.com>
2017-01-31 15:49:39 -0200

    Mostrando o componente genero na tela, exibindo uma lista de genero.

--- src/app/genero/genero.component.ts
+++ src/app/genero/genero.component.ts
@@ -9,7 +9,7 @@ export class GeneroComponent implements OnInit {
 
     constructor() { }
 
-    generos: any[] = [{ nome: 'robson', descricao: 'teste' }];
+    generos: any[] = [{ nome: 'robson', descricao: 'teste' }, { nome: 'robson2', descricao: 'teste2' }];
 
     ngOnInit() {
 
