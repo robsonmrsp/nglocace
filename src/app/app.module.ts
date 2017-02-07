66fd0f87ae48d4d3e0a2a0e007027bb16fc6009c
Robson <robsonmrsp@gmail.com>
2017-02-07 15:03:47 -0200

    Adição do ng-bootstrap da turma do  proprio angular. Uso do ngb-pagination no page-genero.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+
 
 import { AppComponent } from './app.component';
 
@@ -25,6 +27,7 @@ import { HomeComponent } from './home/home.component';
         HomeComponent
     ],
     imports: [
+        NgbModule.forRoot(),
         BrowserModule,
         FormsModule,
         HttpModule,
