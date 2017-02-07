fbf237a99eebda9c1cb76873877489160105d4dc
Robson <robsonmrsp@gmail.com>
2017-02-07 17:27:52 -0200

    Alterando o icone de sort ao click

--- src/app/components/datatable-sort.directive.ts
+++ src/app/components/datatable-sort.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, EventEmitter, Output, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer, EventEmitter, Output, HostListener, Input } from '@angular/core';
+
 import { DatatablePageConfig, Page, Config } from './models/datatable-page-config';
 
 @Directive( {
     selector: '[datatableSort]'
 })
 export class DatatableSortDirective {
-    constructor() { }
 
     @Output() public sortChanged: EventEmitter<any> = new EventEmitter();
     @Input( "datatableSort" ) public datatableConfig: DatatablePageConfig;
     @Input() public sortName: string = '';
     direction: string = '';
+
+    constructor( private el: ElementRef, private render: Renderer ) {
+
+    }
 
     @Input()
     public get config(): DatatablePageConfig {
@@ -24,28 +28,31 @@ export class DatatableSortDirective {
 
     @HostListener( 'click', ['$event'] )
     public onToggleSort( event: any ): void {
+
         if ( event ) {
             event.preventDefault();
         }
-
+        let icon = this.el.nativeElement.querySelector( 'i' )
         switch ( this.direction ) {
             case 'asc':
                 this.direction = 'desc';
                 this.datatableConfig.config.direction = 'desc';
                 this.datatableConfig.config.orderBy = this.sortName;
+                icon.className = "fa fa-sort-desc"
                 break;
             case 'desc':
                 this.direction = '';
                 this.datatableConfig.config.direction = '';
                 this.datatableConfig.config.orderBy = '';
+                icon.className = "fa fa-sort"
                 break;
             default:
                 this.direction = 'asc';
                 this.datatableConfig.config.direction = 'asc';
                 this.datatableConfig.config.orderBy = this.sortName;
+                icon.className = "fa fa-asc"
                 break;
         }
-
 
         this.sortChanged.emit( this.datatableConfig );
     }
